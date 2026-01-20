c43fffd7ae33bd88ebbf1f8b91dc1cba53ed17a3
Manmeet <manmeetkaurnahal1313@gmail.com>
2026-01-20 14:39:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-
-<header class="navbar">
-    <div class="logo">BRAND<span>NAME</span></div>
-    
-    <button class="menu-toggle" id="mobile-menu">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-    </button>
-
-    <nav id="nav-menu">
-        <ul class="nav-links">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </nav>
-    <div class="nav-cta">
-        <a href="#" class="btn-secondary">Get Started</a>
-    </div>
-</header>
+// Simple scroll effect for navbar transparency
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        nav.style.borderBottom = "1px solid rgba(255, 255, 255, 0.1)";
+    } else {
+        nav.style.borderBottom = "none";
+    }
+});
